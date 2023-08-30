9c4aac6f28d5941b1b7e2e5245562ffd7725af8d
Tommy <tommyjaa@hotmail.com>
2023-08-30 15:08:05 +0200

    Fixed redirect when logged in, and made small changes to navbar

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.css'],
 })
-export class LoginPage {
-  constructor(private readonly router: Router) {}
+export class LoginPage implements OnInit {
+  constructor(
+    private readonly router: Router,
+    private readonly userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.userService.user) {
+      this.router.navigateByUrl('/catalog');
+    }
+  }
 
   handleLogin(): void {
     this.router.navigateByUrl('/catalog');
